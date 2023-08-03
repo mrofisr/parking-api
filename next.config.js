46f1f4a6b4c9238248669722ab6e2ba94b772c76
Abdur Rofi <abdur.rofi@mtarget.co>
2023-08-03 23:58:45 +0700

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-});
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-module.exports = withPWA({
-  reactStrictMode: true
-});
+module.exports = nextConfig
